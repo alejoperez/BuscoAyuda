a6f2a3f081ebfcbac314bd13866211a2cc1fa85e
Alejandro <alejoperezgarcia@gmail.com>
2016-02-15 23:36:49 -0500

    df

--- hello/static/src/modules/detail/detail.svc.js
+++ hello/static/src/modules/detail/detail.svc.js
@@ -10,8 +10,8 @@
         this.editDetail = function (independent) {
             return $http({
                 method: 'POST',
-                //url: 'https://ancient-plains-90032.herokuapp.com/detail',
-                url: 'http://127.0.0.1:8000/detail',
+                url: 'https://ancient-plains-90032.herokuapp.com/detail',
+                //url: 'http://127.0.0.1:8000/detail',
                 data:{
                     name: independent.fields.name,
                     last_name: independent.fields.lastName,
@@ -28,16 +28,16 @@
         this.getDetail = function () {
             return $http({
                 method: 'GET',
-                //url: 'https://ancient-plains-90032.herokuapp.com/detail'
-                url: 'http://127.0.0.1:8000/detail'
+                url: 'https://ancient-plains-90032.herokuapp.com/detail'
+                //url: 'http://127.0.0.1:8000/detail'
             });
         }
 
         this.getJobs = function () {
             return $http({
                 method: 'GET',
-                //url: 'https://ancient-plains-90032.herokuapp.com/jobs'
-                url: 'http://127.0.0.1:8000/jobs'
+                url: 'https://ancient-plains-90032.herokuapp.com/jobs'
+                //url: 'http://127.0.0.1:8000/jobs'
             });
         };
 
