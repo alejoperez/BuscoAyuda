d52f68ca45c5425a5934946bb75d2e5ce4dcdffd
alejoperez <alejo@gmail.com>
2016-03-31 22:56:47 -0500

    Paso 1: Titulo de la página (Rojo)

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+from selenium import webdriver
+
+
+class FunctionalTest(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('BuscoAyuda', self.browser.title)
