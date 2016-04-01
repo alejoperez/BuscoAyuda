6da67c95082561c5315802893283c10d2438dd09
alejoperez <alejo@gmail.com>
2016-03-31 23:20:23 -0500

    Paso 2: Registrar independiente (Rojo)

--- hello/static/src/modules/independents/independents.ctrl.js
+++ hello/static/src/modules/independents/independents.ctrl.js
@@ -21,15 +21,15 @@
         };
 
         this.registerIndependent = function(){
-            var name = angular.element('#name').val();
-            var lastName = angular.element('#last_name').val();
-            var job = angular.element('#job').val().trim();
-            var yearsOfExperience = angular.element('#years_of_experience').val();
-            var phoneNumber = angular.element('#phone_number').val();
-            var email = angular.element('#email').val();
-            var imageFileUrl = angular.element('#imageFileUrl').val();
-            var username = angular.element('#username').val();
-            var password = angular.element('#password').val();
+            var name = angular.element('#id_name').val();
+            var lastName = angular.element('#id_last_name').val();
+            var job = angular.element('#id_job').val().trim();
+            var yearsOfExperience = angular.element('#id_years_of_experience').val();
+            var phoneNumber = angular.element('#id_phone_number').val();
+            var email = angular.element('#id_email').val();
+            var imageFileUrl = angular.element('#id_imageFileUrl').val();
+            var username = angular.element('#id_username').val();
+            var password = angular.element('#id_password').val();
             console.log(job);
             if (name && lastName && job && yearsOfExperience && phoneNumber && validarEmail(email)==true && imageFileUrl && username && password){
                 console.log('antes de regirto 2');
--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,5 @@
+import random
+import string
 from unittest import TestCase
 
 # Create your tests here.
@@ -14,3 +16,37 @@ class FunctionalTest(TestCase):
     def test_title(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
+
+def test_register(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_link_text('Registrarte')
+        link.click()
+
+        name = self.browser.find_element_by_id('id_name')
+        name.send_keys('ALejo')
+
+        last_name = self.browser.find_element_by_id('id_last_name')
+        last_name.send_keys('perez')
+
+        years = self.browser.find_element_by_id('id_years_of_experience')
+        years.send_keys('3')
+
+        phone_number = self.browser.find_element_by_id('id_phone_number')
+        phone_number.send_keys('3103211293')
+
+        email = self.browser.find_element_by_id('id_email')
+        email.send_keys('email@eae.dd')
+
+        imageFileUrl = self.browser.find_element_by_id('id_imageFileUrl')
+        imageFileUrl.send_keys('http://evolvedms.com/images/ARP-Logo-EvolvedMS.jpg')
+
+        username = self.browser.find_element_by_id('id_username')
+        #username.send_keys(''.join(random.choice(string.lowercase) for i in range(10)))
+
+        password = self.browser.find_element_by_id('id_password')
+        password.send_keys('password')
+
+        self.browser.find_element_by_id('id_button_register').click()
+        self.browser.implicitly_wait(3)
+
+        self.assertIsNone(self.browser.find_element_by_link_text('id_button_register').click())
